36d401b4523a9d7f3e8f5e5b3fdfa15afa7379ba
romainberger <romain@romainberger.com>
2013-05-09 15:13:18 +0200

    Forgot to bump version ;_;

--- example/js/select-drop.js
+++ example/js/select-drop.js
@@ -3,7 +3,7 @@
  * Simple jQuery plugin to convert select to dropdown
  *
  * https://github.com/romainberger/select-drop
- * @version 0.3
+ * @version 0.4
  * @author: Romain Berger <romain@romainberger.com>
  *
  * Basic Usage:
